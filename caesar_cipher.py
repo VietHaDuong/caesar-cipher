270624c08dd8974a1b04bd40df9319337528f84c
Viet Ha <117793465+VietHaDuong@users.noreply.github.com>
2024-09-20 16:36:24 +0800

    added decode function

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,17 +1,36 @@
 import string
 
-user_input = list(input("Enter a phrase: "))
-shift_number = int(input("Number of shift: "))
 alpha = list(string.ascii_lowercase)
 current_place =''
-encrypt = []
+result = []
 
-for i in user_input:
-    i == alpha.index(i)
-    new_place = (alpha.index(i) + shift_number)
-    if new_place > 25:
-        new_place -= 26
-    new_char = alpha[new_place]
-    encrypt.append(new_char)
+def main():
+    option = input("Encode or decode: ").lower()
+    user_input = list(input("Enter a phrase: ").replace(" ",''))
+    shift_number = int(input("Number of shift: "))
+    if option == "encode":
+        answer = encode(user_input, shift_number)
+        print("".join(answer))
+    elif option == "decode":
+        answer = decode(user_input, shift_number)
+        print("".join(answer))
 
-print("".join(encrypt))
+def encode(user_input, shift_number):
+    for i in user_input:
+        i == alpha.index(i)
+        new_place = (alpha.index(i) + shift_number)
+        if new_place > 25:
+            new_place -= 26
+        new_char = alpha[new_place]
+        result.append(new_char)
+    return result
+
+def decode(user_input, shift_number):
+    for i in user_input:
+        i == alpha.index(i)
+        new_place = (alpha.index(i) - shift_number)
+        new_char = alpha[new_place]
+        result.append(new_char)
+    return result
+
+main()
